6d8738485a5c64365e077ace079bcf3cfd992eec
Mohamed Ubeid <mubaid@vast.ps>
2024-07-27 15:49:12 +0300

    generate login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
   {
     path: 'loader',
     loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
-  },
-  {
+  },  {
     path: 'login',
-    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 
 @NgModule({
